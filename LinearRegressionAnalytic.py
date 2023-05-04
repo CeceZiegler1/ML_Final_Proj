80a66c66096a7e82f10349b6ee0c2b5825a2a4b5
CeceZiegler1 <cziegler@middlebury.edu>
2023-05-04 10:02:14 -0400

    Final Linear Regression source code

--- LinearRegressionAnalytic.py
+++ LinearRegressionAnalytic.py
@@ -1,10 +1,24 @@
 import numpy as np
 
-class LinearRegressionAnalytic():
+class LinearRegressionAnalytic:
+    def __init__(self):
+        self.w = None
+        
+    def fit(self, X, y):
+        X_ = self.pad(X)
 
-    def __init__(self, fit_intercept=True):
-        self.fit_intercept = fit_intercept
+        #initializing weight vector
+        w = np.random.random((X_.shape[1]-1))
+        self.w = w
+        
+        self.alpha = 5
 
+        #Regularizing weight vector
+        I = np.eye(X_.shape[1])
+        # Don't regularize the bias term to avoid underfitting
+        I[0, 0] = 0 
+        self.w = np.linalg.inv(X_.T@X_ + self.alpha*I)@X_.T@y
+        
     def pad(self, X): #I give credit for this method to the help section of the assignment page
         return np.append(X, np.ones((X.shape[0], 1)), 1)
 
@@ -14,25 +28,10 @@ class LinearRegressionAnalytic():
 
         X_ = self.pad(X)
         return X_@w
-
-    def fit(self, X, y):
-
-        '''Input: Feature matrix, X and target vector, y, returns nothing
-
-        This method computes the value of the weight vector, w, using the analytical formula
-        we calculated in class. It then computes the score and keeps track of the score history
-        with an array'''
-
-        X_ = self.pad(X)
-        w = np.random.random((X_.shape[1]-1))#initializing weight vector
-        self.w = w
-
-        self.w = np.linalg.inv(X_.T@X_)@X_.T@y #updating w using analytical formula
-        self.coef_ = self.w[:-1] # Set coef_ attribute to the weight vector, excluding the bias term
-
+        
     def score(self, X, y):
 
-        '''input: this method takes in the feature matrix X and target vector y
+        '''input: feature matrix X and target vector y
 
         output: this method returns a float that is the value of the score
 
@@ -41,6 +40,46 @@ class LinearRegressionAnalytic():
         y_hat = self.predict(X,self.w)
         return 1- ((np.sum((y_hat-y)**2))/(np.sum((y-y.mean())**2)))
 
-    def get_params(self, deep=True):
-        return {'fit_intercept': self.fit_intercept}
+
+# Implementing RFE function
+def rfe(X, y, k):
+
+    '''
+    Input: Feature matrix, X
+           Target vector, y
+           Number of features to select, int, k
+
+    Returns: A list of indicies corresponding to the selected features
+
+    This method performs a recursive reature elimination using the above implemented
+    linear regression method that includes L2 regularization
+    
+    '''
+
+    if X.shape[1] < k:
+        raise ValueError("Not enough features to select.")
+
+    lr = LinearRegressionAnalytic()
+    # initialize the set of selected features to be empty
+    selected = set()
+    # initialize the set of remaining features to be all the features
+    remaining = set(range(X.shape[1]))
+    # iterate k times
+    for i in range(k):
+        # compute the optimal weights for each remaining feature
+        weights = {}
+        for j in remaining:
+            features = list(selected) + [j]
+            X_subset = X.iloc[:, features]
+            lr.fit(X_subset, y)
+            weights[j] = lr.w
+        # select the feature with the smallest absolute weight
+        best_feature = min(weights, key=lambda x: abs(weights[x][-1]))
+        # add the best feature to the set of selected features
+        selected.add(best_feature)
+        # remove the best feature from the set of remaining features
+        remaining.remove(best_feature)
+    # return the indices of the selected features
+    return list(selected)
+
 
